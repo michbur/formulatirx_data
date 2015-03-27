1fc5475863ef51156bb4cc658319fcf3ddd92ba9
michbur <michalburdukiewicz@gmail.com>
2015-03-27 14:18:00 +0100

    data update

--- data_analysis.R
+++ data_analysis.R
@@ -2,8 +2,17 @@ source("read_data.R")
 library(dpcR)
 
 dat <- EGFR[, c("Group.name",
-        "Number.of.positives.in.data.filter.1", 
-        "Number.of.positives.in.data.filter.2", 
-        "Number.of.positives.in.data.filter.3")]
+                "Number.of.positives.in.data.filter.1", 
+                "Number.of.positives.in.data.filter.2", 
+                "Number.of.positives.in.data.filter.3")]
 
 colnames(dat) <- c("group", "f1", "f2", "f3")
+
+lambdas <- t(apply(dat[, -1]/496, 1, dpcR:::fl))
+
+#cumulative probability of having partition with 0 or 1 template molecules
+cum_prob <- t(apply(lambdas, 1, function(column)
+  sapply(column, function(lambda) sum(dpois(c(0, 1), lambda = lambda)))))
+
+#cumulative probability of non-empty wells
+cum_prob[dat[, "group"] != "Empty", ]
