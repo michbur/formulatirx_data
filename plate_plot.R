cda2574184f8c6b58de100d4dee3b3569c6581ac
michbur <michalburdukiewicz@gmail.com>
2015-04-02 22:24:25 +0200

    I can easily read formulatrix data

--- plate_plot.R
+++ plate_plot.R
@@ -10,4 +10,10 @@ dat <- EGFR[, c("Well.row", "Well.column", "Group.name",
 plate_mat <- matrix(0, nrow = 8, ncol = 12)
 inds <- as.matrix(dat[, c("Well.row", "Well.column")]) + 1
 colnames(inds) <- c("row", "column")
-plate_mat[inds] <- dat[, "Number.of.positives.in.data.filter.2"]
+plate_mat[inds] <- dat[, "Number.of.positives.in.data.filter.3"]
+
+formul <- create_dpcr(do.call(c, lapply(1L:ncol(plate_mat), function(i) 
+  plate_mat[, i])), length(plate_mat), type = "nm", adpcr = TRUE)
+
+plot_panel(formul, 8, 12)
+
